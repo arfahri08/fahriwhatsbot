b44563c17589164c58a3245f2f568ed4ee9cabd0
arfahri08 <157792725+arfahri08@users.noreply.github.com>
2026-05-04 22:58:33 +0700

    Update afk.js

--- modules/afk.js
+++ modules/afk.js
@@ -1,23 +1,23 @@
-addCommand({ pattern: "^afk$", access: "sudo", desc: "_Turn on or off AFK mode._" }, async (msg, match, sock, rawMessage) => {
+addCommand({ pattern: "^afk$", access: "sudo", desc: "_Mengaktifkan atau menonaktifkan mode AFK._" }, async (msg, match, sock, rawMessage) => {
     const grupId = msg.key.remoteJid;
     const afkMessage = global.database.afkMessage;
     
     if (afkMessage.active) {
         global.database.afkMessage.active = false;
         if (msg.key.fromMe) {
-            return await sock.sendMessage(grupId, { text: "_✅ AFK mode disabled successfully._", edit: msg.key });
+            return await sock.sendMessage(grupId, { text: "_✅ Mode AFK USERBOT Fahri berhasil dinonaktifkan._", edit: msg.key });
         } else {
-            return await sock.sendMessage(grupId, { text: "_✅ AFK mode disabled successfully._"}, { quoted: rawMessage.messages[0] });
+            return await sock.sendMessage(grupId, { text: "_✅ Mode AFK USERBOT Fahri berhasil dinonaktifkan._"}, { quoted: rawMessage.messages[0] });
         }
     } else {
         global.database.afkMessage.active = true;
         if (afkMessage.type == "text" && afkMessage.content == "") {
-            global.database.afkMessage.content = "_I'm currently AFK! Please contact me later._";
+            global.database.afkMessage.content = "_USERBOT Fahri sedang AFK! Silakan hubungi lagi nanti._";
         }
         if (msg.key.fromMe) {
-            return await sock.sendMessage(grupId, { text: "_✅ AFK mode enabled successfully._", edit: msg.key });
+            return await sock.sendMessage(grupId, { text: "_✅ Mode AFK USERBOT Fahri berhasil diaktifkan._", edit: msg.key });
         } else {
-            return await sock.sendMessage(grupId, { text: "_✅ AFK mode enabled successfully._"}, { quoted: rawMessage.messages[0] });
+            return await sock.sendMessage(grupId, { text: "_✅ Mode AFK USERBOT Fahri berhasil diaktifkan._"}, { quoted: rawMessage.messages[0] });
         }
     }
 });
